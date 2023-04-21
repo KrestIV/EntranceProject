6acc92f9ff6de62d5b8ca4621a2de9cddac9146d
Krest_IV <krestsov.iv@yandex.ru>
2023-04-22 02:27:29 +0300

    Serialization test. GitIgnore saving file.

--- src/Counter.java
+++ src/Counter.java
@@ -1,5 +1,16 @@
 import java.io.Serializable;
 
 public class Counter implements Serializable {
-    long currentCounter;
+    private static final long serialVersionUID = 1L;
+    private long currentCounter;
+    public Counter(){
+        this.currentCounter = 0L;
+    }
+
+    public long getCurrentCounter() {
+        return currentCounter;
+    }
+    public void setCurrentCounter(long currentCounter){
+        this.currentCounter = currentCounter;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException{
+        Counter globalCounter = new Counter();
+        System.out.println("current count of the global counter is " + globalCounter.getCurrentCounter());
+
+        FileOutputStream fileOutputStream = new FileOutputStream("D:\\javaProjects\\EntranceProject\\src\\savedGlobalCounter.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        objectOutputStream.writeObject(globalCounter);
+
+        objectOutputStream.close();
+        System.out.println("Stream closed!");
     }
 }
