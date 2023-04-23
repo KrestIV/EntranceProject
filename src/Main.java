324a59727856caa1972655340c5a06d9da0f1f6e
Krest_IV <krestsov.iv@yandex.ru>
2023-04-23 03:16:39 +0300

    Deserialization + generic class for ser/deser containment

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,27 @@
-import java.io.IOException;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        Counter globalCounter = new Counter();
+    final static String SAVE_FILE_ADDRESS = "src\\savedGlobalCounter.txt";
+    public static void main(String[] args) throws IOException, ClassNotFoundException{
+        Counter globalCounter;
+        SaveLoadCounter<Counter> saveLoadCounter = new SaveLoadCounter<>();
+        try {
+            globalCounter = saveLoadCounter.loadFromFile(SAVE_FILE_ADDRESS);
+        }catch (IOException ex){
+            System.out.println("IOExeption has occured during execution. We're in trouble, captain!");
+        }catch(ClassNotFoundException ex){
+            System.out.println("ClassNotFoundException has occured during execution. We're in trouble, captain!");
+        }
+
+        globalCounter.setCurrentCounter(globalCounter.getCurrentCounter() + 1L);
+
         System.out.println("current count of the global counter is " + globalCounter.getCurrentCounter());
+        try {
+            saveLoadCounter.SaveToFile(globalCounter, SAVE_FILE_ADDRESS);
+            System.out.println("Stream closed!");
+        }catch (IOException ex){
+            System.out.println("IOExeption has occured during execution. We're in trouble, captain!");
+        }
 
-        FileOutputStream fileOutputStream = new FileOutputStream("D:\\javaProjects\\EntranceProject\\src\\savedGlobalCounter.txt");
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-
-        objectOutputStream.writeObject(globalCounter);
-
-        objectOutputStream.close();
-        System.out.println("Stream closed!");
     }
 }
